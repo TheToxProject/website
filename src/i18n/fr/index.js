d77a15054b9ff18c1c318a7453221bb4ec71ef82
SkyzohKey <skyzohkey@protonmail.com>
2018-03-19 21:41:32 +0100

    Add a Language switcher component

--- src/i18n/fr/index.js
+++ src/i18n/fr/index.js
@@ -2,10 +2,12 @@ import menu from "./menu.json";
 import footer from "./footer.json";
 import indexPage from "./indexPage.json";
 import downloadPage from "./downloadPage.json";
+import commons from "./commons.json";
 
 export default {
   menu,
   footer,
   indexPage,
-  downloadPage
+  downloadPage,
+  commons
 };
