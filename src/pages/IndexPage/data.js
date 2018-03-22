c9caee2143fb5975e626b37928bfccdc272dc632
SkyzohKey <skyzohkey@protonmail.com>
2018-03-22 22:45:16 +0100

    Refactored the featured in section.

--- src/pages/IndexPage/data.js
+++ src/pages/IndexPage/data.js
@@ -88,3 +88,45 @@ export const getFeatures = t => {
     ]
   };
 };
+
+export const getFeaturedIns = t => {
+  return [
+    {
+      name: "Wired",
+      imageUrl: "https://svgshare.com/i/5wj.svg"
+    },
+    {
+      name: "Linux.com",
+      imageUrl: "https://i.imgur.com/aDlElFF.png"
+    },
+    {
+      name: "CCN",
+      imageUrl:
+        "https://248qms3nhmvl15d4ne1i4pxl-wpengine.netdna-ssl.com/wp-content/themes/cryptocoinsnews/dist/images/logo.svg"
+    },
+    {
+      name: "Korben",
+      imageUrl: "https://i.imgur.com/V79HOKk.png",
+      size: { width: 40, height: 40 }
+    },
+    {
+      name: "Xakep",
+      imageUrl: "https://xakep.ru/wp-content/uploads/2016/09/xakep_logo_ws.png",
+      size: { width: 40, height: 40 }
+    },
+    {
+      name: "MakeUseOf",
+      imageUrl: "https://svgshare.com/i/5w0.svg",
+      size: { width: 75, height: 40 }
+    },
+    {
+      name: "The CoinTelegraph",
+      imageUrl: "https://i.imgur.com/ktJAYGK.png"
+    },
+
+    {
+      name: "NetzPolitik",
+      imageUrl: "https://i.imgur.com/xgjnSHi.png"
+    }
+  ];
+};
